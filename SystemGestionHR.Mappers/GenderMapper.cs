2e16e2be812b7290aa8d6c92c236efeb28c88e89
laubrebr06 <113524405+laubrebr06@users.noreply.github.com>
2025-06-18 04:11:10 -0600

    Add files via upload

---
+++ SystemGestionHR.Mappers/GenderMapper.cs
@@ -0,0 +1,27 @@
+﻿using SystemGestionHR.Mappers.Interfaces;
+using SystemGestionHR.Data.DTO;
+using SystemGestionHR.Data.Models;
+
+namespace SystemGestionHR.Mappers
+{
+    public class GenreMapper : IGenreMapper
+    {
+
+        public GenreDTO MapToGenreDTO(Genre genre)
+        {
+
+            GenreDTO genreDTO = new GenreDTO();
+            genreDTO.GenreId = genre.GenreId;
+            genreDTO.Nom = genre.Nom;
+            return genreDTO;
+        }
+
+        public Genre MapToGenre(GenreDTO genreDTO)
+        {
+            Genre genre = new Genre();
+            genre.GenreId = genreDTO.GenreId;
+            genre.Nom = genreDTO.Nom;
+            return genre;
+        }
+    }
+}
